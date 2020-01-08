9b7c8ea141227f52d9b80b62547acba2a23f89bc
AM-77 <amine.griche77@gmail.com>
2020-01-08 17:28:46 +0100

    retriieved data from json file

--- src/main/java/com/lonix/det/controllers/MainController.java
+++ src/main/java/com/lonix/det/controllers/MainController.java
@@ -2,12 +2,26 @@ package com.lonix.det.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lonix.det.models.Machines;
+import java.io.File;
 
 @RestController
 public class MainController {
 
 	@GetMapping("/")
 	public String cool(){
-		return "What The Fuck";
+
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			Machines machines = objectMapper.readValue(new File("src/main/resources/data/machines.json"), Machines.class);
+
+			return "Data retrieved: there is " + machines.getMachines().size() + " machine.";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "There was an error in the server.";
+		}
+
 	}
+
 }
