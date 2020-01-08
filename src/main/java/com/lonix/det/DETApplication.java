be35032ab2b3514bfe383627347a77761f3f38fc
AM-77 <amine.griche77@gmail.com>
2020-01-08 16:48:35 +0100

    added the App Entry

---
+++ src/main/java/com/lonix/det/DETApplication.java
@@ -0,0 +1,13 @@
+package com.lonix.det;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DETApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DETApplication.class, args);
+	}
+
+}
