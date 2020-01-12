150580d35b4fcc707455d5f6e8fb5d929e168e61
LoneXHuB <khelifi.louness@live.com>
2020-01-12 20:26:29 +0100

    search rest and others

--- src/main/java/com/lonex/services/JsonFileReaderService.java
+++ src/main/java/com/lonex/services/JsonFileReaderService.java
@@ -1,6 +1,7 @@
 package com.lonex.services;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -40,6 +41,63 @@ public class JsonFileReaderService {
 			return null;
 		}
 	}
+	public List<MachineCategory> getSearchMachineCategoryList(String searchQuery){
+		
+		List<MachineCategory> allMachines = this.getFullMachineCategoryList();
+		List<MachineCategory> searchResult = new ArrayList<MachineCategory> ();
+		
+		char[] searchQueryArray = searchQuery.toUpperCase().toCharArray();
+		
+		
+		
+		for(MachineCategory machine : allMachines) {
+			boolean allMatch = true;
+			char[] mapNameArray = machine.getMapName().toUpperCase().toCharArray();
+			int index = 0;
+
+			for(char queryChar : searchQueryArray)
+			{
+			
+				if(queryChar != mapNameArray[index])
+				{
+					allMatch=false;
+					break;
+				}
+			
+			  index++;
+			}
+			
+			if(allMatch)
+			{
+				System.out.println(machine.getMapName() + " valide !");
+				searchResult.add(machine);
+			}
+			if(searchQueryArray.length==0)
+				return allMachines;
+			
+		}
+		
+		return searchResult;
+	}
+	
+	
+	public List<MachineCategory> getFullMachineCategoryList() {
+		List<MachineCategory> centres;
+		List<MachineCategory> machines;
+		
+		try {
+				centres = objectMapper.readValue(new File("src/main/resources/data/centre.json"), new TypeReference<List<MachineCategory>>(){});
+				machines = objectMapper.readValue(new File("src/main/resources/data/tour.json"), new TypeReference<List<MachineCategory>>(){});
+			}catch(Exception e) {e.printStackTrace(); return null;}
+		
+		for(MachineCategory machine : centres)
+		{
+			machines.add(machine);
+		}
+		
+		return machines;
+	}
+	
 
 	public List<Machine> getMachinesFromJson() {
 		// TODO Auto-generated method stub
