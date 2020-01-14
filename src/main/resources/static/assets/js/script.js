b67ef5cc2c16156727111e61f4a43110bf9c1cce
AM-77 <amine.griche77@gmail.com>
2020-01-14 23:40:10 +0100

    display machine infos

--- src/main/resources/static/assets/js/script.js
+++ src/main/resources/static/assets/js/script.js
@@ -1,6 +1,7 @@
+import { format_machine } from "./machine.template.js"
 $(document).ready(() => {
-    //* The sliders
-    let _slider = (selector, items_0, items_900, items_1200) => {
+    //* The machines slider
+    const slider = (selector, items_0, items_900, items_1200) => {
         $(selector).owlCarousel({
             nav: true,
             navText: ['<img src="/assets/images/arrowhead-left.svg" alt="prev" />', '<img src="/assets/images/arrowhead-right.svg" alt="next" />'],
@@ -9,7 +10,7 @@ $(document).ready(() => {
             pullDrag: true,
             rtl: false,
             loop: true,
-            margin: 00,
+            margin: 1,
             items: 3,
             autoplay: true,
             smartSpeed: 1000,
@@ -28,12 +29,10 @@ $(document).ready(() => {
             }
         })
     }
-
-    //* The machines slider
-    _slider('.machines .owl-carousel', 1, 2, 3)
+    slider('.machines .owl-carousel', 1, 2, 3)
 
     //* active links 
-    let set_active_page = () => {
+    const set_active_page = () => {
         let links = ["home", "centre", "tour", "contact"]
         links.forEach(link => $(".navbar a.link." + link).removeClass("active"))
         switch (location.pathname) {
@@ -55,59 +54,66 @@ $(document).ready(() => {
     }
     set_active_page()
 
-    // grid / list view
+    //* grid / list view
     if ($(".machines-container .sidebar")) {
-        $(".machines-container .sidebar img.grid-view").click(() => {
-            $(".machines-container .sidebar").removeClass("list").addClass("grid")
-        })
+        $(".machines-container .sidebar img.grid-view").click(() => { $(".machines-container .sidebar").removeClass("list").addClass("grid") })
+        $(".machines-container .sidebar img.list-view").click(() => { $(".machines-container .sidebar").removeClass("grid").addClass("list") })
+    }
 
-        $(".machines-container .sidebar img.list-view").click(() => {
-            $(".machines-container .sidebar").removeClass("grid").addClass("list")
+    let active_item = {}
+    //* select a mechine from
+    const set_active_item = () => {
+        $(".sidebar .machines .machine").click(function (e) {
+            $(".sidebar .machines .machine.active").removeClass("active")
+            $(this).addClass("active")
+            const machine_name = $(this).find("span").text()
+            active_item.machine_img = $(this).find("img").attr("src")
+            fetch(`${location.href}/${$(this).attr("data-map-name")}`)
+                .then(res => res.json())
+                .then(res => {
+                    console.log(res)
+                    $(".machines-container .content").html(format_machine(machine_name, res))
+                })
         })
     }
 
+    let machines_list = []
     //* display sidbar machines
-    let machines = $(".sidebar ._machines .machine")
-
-    let format_html = (machines) => {
+    const format_html = (machines) => {
         let machines_html = ""
-
-        for (let i = 0; i < machines.length; i++) {
-            let img_src = $(machines[i]).find("img").attr("src").trim()
-            let machine_name = $(machines[i]).find("span").text().trim()
-
-            machines_html += `<div class="machine"><div><img src="${img_src}" alt="${machine_name} machine" /><span>${machine_name}</span></div></div>`
-        }
-        return machines_html
+        machines.forEach(machine => {
+            machines_html += `<div class="machine" data-map-name=${machine.mapName}><div><img src="${location.origin}/${machine.imageName}" alt="${machine.machineName} machine" /><span>${machine.machineName}</span></div></div>`
+        })
+        if (machines_html === "") machines_html = `<div class="machine no-machine"><span>No Machine Was Found</span></div></div>`
+        $(".sidebar .machines").html(machines_html)
+        set_active_item()
     }
-
-    $(".sidebar .machines").html(format_html(machines))
 
     //* search a machine
     let search_input = $(".machines-container .sidebar .head .search input")
 
     search_input.keyup(e => {
         let value = e.target.value.trim().toLowerCase()
-        if (value !== "") {
-            $(".sidebar .machines").html(format_html(find_machine(value, machines)))
-        } else {
-            $(".sidebar .machines").html(format_html(machines))
-        }
+        if (value !== "") format_html(find_machine(value, machines_list))
+        else format_html(machines_list)
     })
 
-    let find_machine = (machine_name, machines) => {
+    const find_machine = (machine_name, machines) => {
         let found_machines = []
         for (let i = 0; i < machines.length; i++)
-            if ($(machines[i]).find("span").text().trim().toLowerCase().indexOf(machine_name) >= 0)
-                found_machines.push($(machines[i]))
+            if (machines[i].machineName.toLowerCase().indexOf(machine_name) >= 0)
+                found_machines.push(machines[i])
         return found_machines
     }
 
-    // select a mechine from
-    $(".sidebar .machines .machine").click(function (e) {
-        console.log($(this));
-        $(".sidebar .machines .machine.active").removeClass("active")
-        $(this).addClass("active")
-    })
-
+    // fetching data from the API
+    const display_sidebar = (pathname) => {
+        fetch(`${location.origin}/Search/${pathname.slice(pathname.lastIndexOf("/") + 1)}`)
+            .then(res => res.json())
+            .then(res => {
+                machines_list = res
+                format_html(machines_list)
+            })
+    }
+    display_sidebar(location.pathname)
 })
