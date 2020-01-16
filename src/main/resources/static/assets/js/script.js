215ec2014d48536819f2d548c6b08949a6fed4a4
AM-77 <amine.griche77@gmail.com>
2020-01-16 21:29:26 +0100

    updated css & js

--- src/main/resources/static/assets/js/script.js
+++ src/main/resources/static/assets/js/script.js
@@ -1,5 +1,16 @@
-import { format_machine } from "./machine.template.js"
+import {
+    format_machine
+} from "./machine.template.js"
+
 $(document).ready(() => {
+
+    //* Scroll to about
+    $(".home-container .logo img.go-down").click(function () {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(".about-us").offset().top
+        }, 700)
+    })
+
     //* The machines slider
     const slider = (selector, items_0, items_900, items_1200) => {
         $(selector).owlCarousel({
@@ -31,6 +42,47 @@ $(document).ready(() => {
     }
     slider('.machines .owl-carousel', 1, 2, 3)
 
+    //* Show and Hide The Sidebar
+    const hide_sidebar = () => {
+        $(".machines-container .sidebar").animate({
+            left: "-330px"
+        }, 350, () => {})
+        $(".machines-container .content").animate({
+            width: "100%",
+            marginLeft: "0px"
+        }, 350, () => {})
+        $(".header .show-sidebar").css({
+            display: "block"
+        }).animate({
+            opacity: 1
+        }, 350, () => {})
+    }
+
+    const show_sidebar = () => {
+        $(".machines-container .sidebar").animate({
+            left: "0px"
+        }, 350, () => {})
+        $(".machines-container .content").animate({
+            width: "calc(100% - 300px)",
+            marginLeft: "300px"
+        }, 350, () => {})
+        $(".header .show-sidebar").animate({
+            opacity: 0
+        }, 350, () => {
+            $(".header .show-sidebar").css({
+                display: "none"
+            })
+        })
+    }
+
+    $(".machines-container .sidebar .head .hide-sidebar").click(hide_sidebar)
+    $(".header .show-sidebar").click(show_sidebar)
+
+    //* Show and Hide The Mobile Navbar
+    $(".container .header .nav-btn").click(() => {
+        $(".container").hasClass("mobile") ? $(".container").removeClass("mobile") : $(".container").addClass("mobile")
+    })
+
     //* active links 
     const set_active_page = () => {
         let links = ["home", "centre", "tour", "contact"]
@@ -38,6 +90,9 @@ $(document).ready(() => {
         switch (location.pathname) {
             case "/":
                 $(".navbar a.link.home").addClass("active")
+                $(".header .show-sidebar").css({
+                    display: "none"
+                })
                 break;
             case "/Category/centre":
                 $(".navbar a.link.centre").addClass("active")
@@ -47,6 +102,9 @@ $(document).ready(() => {
                 break;
             case "/contact":
                 $(".navbar a.link.contact").addClass("active")
+                $(".header .show-sidebar").css({
+                    display: "none"
+                })
                 break;
             default:
                 break;
@@ -56,30 +114,45 @@ $(document).ready(() => {
 
     //* grid / list view
     if ($(".machines-container .sidebar")) {
-        $(".machines-container .sidebar img.grid-view").click(() => { $(".machines-container .sidebar").removeClass("list").addClass("grid") })
-        $(".machines-container .sidebar img.list-view").click(() => { $(".machines-container .sidebar").removeClass("grid").addClass("list") })
+        $(".machines-container .sidebar img.grid-view").click(() => {
+            $(".machines-container .sidebar").removeClass("list").addClass("grid")
+        })
+        $(".machines-container .sidebar img.list-view").click(() => {
+            $(".machines-container .sidebar").removeClass("grid").addClass("list")
+        })
     }
 
-    let active_item = {}
     //* select a mechine from
     const set_active_item = () => {
         $(".sidebar .machines .machine").click(function (e) {
             $(".sidebar .machines .machine.active").removeClass("active")
             $(this).addClass("active")
+            hide_sidebar()
             const machine_name = $(this).find("span").text()
-            active_item.machine_img = $(this).find("img").attr("src")
             fetch(`${location.href}/${$(this).attr("data-map-name")}`)
                 .then(res => res.json())
                 .then(res => {
-                    console.log(res)
                     $(".machines-container .content").html(format_machine(machine_name, res))
                 })
         })
     }
 
+    // active the first machine
+    const init_machine = () => {
+        location.pathname === "/Category/tour" ? $(".machines-container .sidebar .head p").text("cnc lathe") : $(".machines-container .sidebar .head p").text("machining center")
+        let machine = $(".sidebar .machines .machine")[0]
+        $(machine).addClass("active")
+        const machine_name = $(machine).find("span").text()
+        fetch(`${location.href}/${$(machine).attr("data-map-name")}`)
+            .then(res => res.json())
+            .then(res => {
+                $(".machines-container .content").html(format_machine(machine_name, res))
+            })
+    }
+
     let machines_list = []
     //* display sidbar machines
-    const format_html = (machines) => {
+    const format_html = (machines, callback) => {
         let machines_html = ""
         machines.forEach(machine => {
             machines_html += `<div class="machine" data-map-name=${machine.mapName}><div><img src="${location.origin}/${machine.imageName}" alt="${machine.machineName} machine" /><span>${machine.machineName}</span></div></div>`
@@ -87,6 +160,7 @@ $(document).ready(() => {
         if (machines_html === "") machines_html = `<div class="machine no-machine"><span>No Machine Was Found</span></div></div>`
         $(".sidebar .machines").html(machines_html)
         set_active_item()
+        callback()
     }
 
     //* search a machine
@@ -94,8 +168,8 @@ $(document).ready(() => {
 
     search_input.keyup(e => {
         let value = e.target.value.trim().toLowerCase()
-        if (value !== "") format_html(find_machine(value, machines_list))
-        else format_html(machines_list)
+        if (value !== "") format_html(find_machine(value, machines_list), init_machine)
+        else format_html(machines_list, init_machine)
     })
 
     const find_machine = (machine_name, machines) => {
@@ -112,8 +186,9 @@ $(document).ready(() => {
             .then(res => res.json())
             .then(res => {
                 machines_list = res
-                format_html(machines_list)
+                format_html(machines_list, init_machine)
             })
     }
-    display_sidebar(location.pathname)
+
+    if (location.pathname.indexOf("/Category") > -1) display_sidebar(location.pathname)
 })
